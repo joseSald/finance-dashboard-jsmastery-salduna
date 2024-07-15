d7ef0c4a6e87046de7753fc9f9e942f56e2cea8f
Pedro Guemes <saldunajose@gmail.com>
2024-07-15 20:15:53 -0300

    Implement plaid link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,6 +21,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 
@@ -52,7 +53,20 @@ const AuthForm = ({ type }: { type: string }) => {
             //and create a Plaid Link Token
 
             if (type === 'sign-up') {
-                const newUser = await signUp(data)
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password,
+
+                }
+                const newUser = await signUp(userData)
 
                 setUser(newUser)
             }
@@ -64,7 +78,7 @@ const AuthForm = ({ type }: { type: string }) => {
                     }
                 );
                 if (response) {
-                    router.push('/')
+                    // router.push('/')
                 }
             }
 
@@ -101,7 +115,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </header>
             {user ? (
                 <div className="flex flex-col gap-4">
-                    {/* PlaidLink component */}
+                    <PlaidLink user={user} variant='primary' />
                 </div>
             ) : (
                 <>
@@ -138,7 +152,7 @@ const AuthForm = ({ type }: { type: string }) => {
                                             <CustomInput control={form.control} name='dateOfBirth' label='Date of birth:'
                                                 placeholder='yyyy-mm-dd'
                                             />
-                                            <CustomInput control={form.control} name='socialSecurityNumber' label='SSN:'
+                                            <CustomInput control={form.control} name='ssn' label='SSN:'
                                                 placeholder='12345678'
                                             />
                                         </div>
@@ -176,8 +190,7 @@ const AuthForm = ({ type }: { type: string }) => {
                         </Link>
                     </footer>
                 </>
-            )
-            }
+            )}
         </section>
     )
 }
